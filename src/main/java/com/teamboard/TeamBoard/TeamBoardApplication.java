7e06dc1f8846a2149ad8453b4f279980958281cd
YC98 <ssy5577@naver.com>
2021-10-29 22:11:53 +0900

    프로젝트 기본 생성 후 초기상태 푸시

---
+++ src/main/java/com/teamboard/TeamBoard/TeamBoardApplication.java
@@ -0,0 +1,13 @@
+package com.teamboard.TeamBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamboard/TeamBoard/TeamBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamboard.TeamBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
